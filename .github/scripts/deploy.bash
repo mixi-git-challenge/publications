8f4fc594ee7ae5d4862b388916a5f33ea93a7f66
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-09-24 18:57:04 +0900

    CI: add GitHub Actions to deploy

---
+++ .github/scripts/deploy.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+mkdir -p /root/.ssh
+ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
+echo "${DEPLOY_KEY}" > /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+
+# commit the assets in docs/ if changed, and push to GitHub using SSH
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+git remote set-url origin git@github.com:${GITHUB_REPOSITORY}.git
+
+git status
+git add docs
+git diff --staged --quiet || git commit -m "Update docs by GitHub Actions"
+git push origin ${GITHUB_REF}
